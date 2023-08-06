d45524992d981c851becdad9f6b6e4d8679905fb
danx <rushn24@gmail.com>
2023-08-05 21:32:11 -0400

    post and track score on front end

--- static/app.js
+++ static/app.js
@@ -6,13 +6,19 @@ class BoggleGame {
         this.board = $('#' + boardId);
         // to store validated words that have already been found and scored
         this.words = new Set();
+        this.score = 0
 
         $(".new-word", this.board).on("submit", this.handleSubmit.bind(this));
     }
 
     // Show message based on word status. 
     showMessage(msg, cls) {
-        $('msg', this.board).text(msg).removeClass().addClass(`msg ${cls}`)
+        $('.msg', this.board).text(msg).removeClass().addClass(`msg ${cls}`)
+    }
+
+    // show the current score
+    showScore() {
+        $('.score', this.board).text(this.score)
     }
 
     // Handle submission of a word
@@ -22,6 +28,7 @@ class BoggleGame {
         const $word = $(".word", this.board);
     
         let word = $word.val();
+        console.log(word)
         if (!word) return;
         if (this.words.has(word)) {
             this.showMessage(`Already found ${word}`)
@@ -29,8 +36,18 @@ class BoggleGame {
 
             //check server for validity
         const resp = await axios.get("/check-word", { params: {word: word }});
-        console.log(resp);
-        console.log('Hello')
+        const result = resp.data.result
+        if(result === 'not-word'){
+            this.showMessage(`${word} is not a valid English word`, 'err');
+        } else if(result === 'not-on-board'){
+            this.showMessage(`${word} is not a valid word on this board`, 'err');
+        } else {
+            this.score += word.length;
+            console.log(this.score)
+            this.showScore(); 
+            this.words.add(word);
+            this.showMessage(`Added: ${word}`, 'ok');
+        }
     }
 
     
